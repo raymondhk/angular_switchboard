ace5b60276153ff18fedf53a8c56db5f51654fa5
Raymond Kim <ray.kim09@gmail.com>
2017-10-11 16:15:24 -0400

    completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switch: object = {
+    btn1: true,
+    btn2: true,
+    btn3: true,
+    btn4: true,
+    btn5: true,
+    btn6: true,
+    btn7: true,
+    btn8: true,
+    btn9: true,
+    btn10: true,
+  };
+  board: Array<string> = ['btn1', 'btn2', 'btn3', 'btn4', 'btn5', 'btn6', 'btn7', 'btn8', 'btn9', 'btn10'];
+  click(btn) {
+    if (this.switch[btn] === true) {
+      this.switch[btn] = false;
+    }
+    else {
+      this.switch[btn] = true;
+    }
+  }
 }
